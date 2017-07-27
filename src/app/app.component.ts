3c4fe12ff4afe29cce0e363bfd2b380e253407b4
tj <tang123jian@126.com>
2017-07-27 17:56:28 +0800

    最小化

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { Component, ViewChild, ViewContainerRef, OnInit } from '@angular/core';
-import { DialogService } from './global/dialog.service';
-import { StateService } from '@uirouter/core';
-import { AuthService } from './global/auth.service';
-import { AppConfigService } from './global/app-config.service';
+import {Component, ViewChild, ViewContainerRef, OnInit} from '@angular/core';
 
 /**
  * This is the main app component for an authenticated user.
@@ -13,67 +9,13 @@ import { AppConfigService } from './global/app-config.service';
  */
 @Component({
   selector: 'app-root',
-  template: `
-    <div #dialogdiv></div>
-    <div class="navheader">
-      <ul *ngIf="isAuthenticated" class="nav nav-tabs">
-
-        <li uiSrefActive="active"> <a uiSref="mymessages" role="button"> Messages </a> </li>
-        <li uiSrefActive="active"> <a uiSref="contacts" role="button"> Contacts </a> </li>
-        <li uiSrefActive="active"> <a uiSref="prefs" role="button"> Preferences </a> </li>
-
-        <li class="navbar-right">
-          <button class="btn btn-primary fa fa-home" uiSref="home"></button>
-          <button style="margin-right: 15px;" class="btn btn-primary" uiSref="mymessages.compose">
-            <i class="fa fa-envelope"></i> New Message
-          </button>
-        </li>
-
-        <li class="navbar-text navbar-right logged-in-user" style="margin: 0.5em 1.5em;">
-          <div>
-            {{emailAddress}} <i class="fa fa-chevron-down"></i>
-            <div class="hoverdrop">
-              <button class="btn btn-primary" (click)="logout()">Log Out</button>
-            </div>
-          </div>
-        </li>
-
-      </ul>
-    </div>
-
-    <ui-view></ui-view>
-`
-  ,
+  templateUrl: './app.component.html',
   styles: []
 })
 export class AppComponent implements OnInit {
-  @ViewChild('dialogdiv', {read: ViewContainerRef}) dialogdiv;
 
-  // data
-  emailAddress;
-  isAuthenticated;
-
-  constructor(appConfig: AppConfigService,
-              public authService: AuthService,
-              public $state: StateService,
-              private dialog: DialogService
-  ) {
-    this.emailAddress = appConfig.emailAddress;
-    this.isAuthenticated = authService.isAuthenticated();
+  ngOnInit(): void {
   }
 
-  ngOnInit() {
-    this.dialog.vcRef = this.dialogdiv;
-  }
 
-  show() {
-    this.dialog.confirm('foo');
-  }
-
-  logout() {
-    const { authService, $state } = this;
-    authService.logout();
-    // Reload states after authentication change
-    return $state.go('welcome', {}, { reload: true });
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './welcome.component';
-import { LoginComponent } from './login.component';
-import { HomeComponent } from './home.component';
 import { UIRouterModule, UIView } from '@uirouter/angular';
 import { APP_STATES } from './app.states';
-import { GlobalModule } from './global/global.module';
-import { routerConfigFn } from './router.config';
+import { MainComponent } from './main/main.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent,
-    LoginComponent,
-    HomeComponent
+    MainComponent,
+    WelcomeComponent
   ],
   imports: [
     UIRouterModule.forRoot({
       states: APP_STATES,
       useHash: true,
-      otherwise: { state: 'home' },
-      config: routerConfigFn,
+      otherwise: { state: 'app' }
     }),
-    GlobalModule,
     BrowserModule,
     FormsModule,
     HttpModule
